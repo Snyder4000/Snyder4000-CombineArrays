0bf2e8bdb8b3924ce3e61a40a800a4d83e280556
Mark Snyder <Snyder4000@yahoo.com>
2023-03-13 11:53:31 +0000

    Completed Lab CombineArrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,11 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int a1= arr1.length;
+        int a2 = arr2.length;
+        int[] newArray = new int[a1 + a2];
+        System.arraycopy(arr1, 0, newArray, 0, a1);
+        System.arraycopy(arr2, 0, newArray, a1, a2);
+        return newArray;
     }
 }
